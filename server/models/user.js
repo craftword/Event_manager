79460d1095bc7f58c5ea21f98de2c58ae3e40a21
Abimbola Olaitan <craftwordltd@gmail.com>
2017-11-23 13:40:03 +0100

    modify user.js

--- server/models/user.js
+++ server/models/user.js
@@ -36,7 +36,7 @@ const Users = (sequelize, DataTypes) => {
        Users.hasMany(models.Centers, { as: 'Center', foreignKey: 'userId' });
   };    
 
-    return Users;Location
+    return Users;
 };
 
 
