0ed27d236d7c46682d4e200638479e5634783d05
Abimbola Olaitan <craftwordltd@gmail.com>
2017-11-23 17:53:01 +0100

    modify travis and users js

--- server/test/users.js
+++ server/test/users.js
@@ -1,4 +1,3 @@
-process.env.NODE_ENV = 'test';
 
 import request from "supertest";
 import { expect } from 'chai'
@@ -7,31 +6,10 @@ import app from "../../app";
 const Users = models.Users;
 const server = request.agent("http://localhost:9000");
 
+
 //Our parent block
 describe('API for SignIn and SignUp', () => {    
-    describe('Test for Models', ()=> {
-        it('respond with json after signup', (done)=> {
-            const prob = {
-                username:"dummy777", 
-                password:"dummy100",
-                fullname:"dummy Dum",
-                email:"mummy@gmail.com",
-                phone:"08967689609"            
-            };
-            server
-                .post('/api/v1/user/signup')
-                .send(prob)
-                .end((err,res)=> {
-                    const data = JSON.parse(res.text);
-                    expect(res.status).to.equal(201);
-                    expect(data.username).to.equal("dummy777");
-                    expect(data.fullname).to.equal("dummy Dum");
-                    expect(data.email).to.equal("mummy@gmail.com");
-                   // expect(data.phone).to.equal("08967689609");
-                    done();
-                });
-        });
-        
+    describe('Test for Models', ()=> {       
         it('respond with a 404 error when signUp with a register username', (done)=> {
             const prob = {
                 username:"dummy777", 
